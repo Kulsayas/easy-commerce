a605cf50582211c603ad87cd7e53118b1906d99b
Kulsayas <Kulsaya.see@gmail.com>
2025-03-31 22:46:21 +0700

    feat: add product card list and hero banner

--- src/views/user/HomeView.vue
+++ src/views/user/HomeView.vue
@@ -3,5 +3,46 @@ import UserLayout from "@/layouts/UserLayout.vue";
 </script>
 
 <template>
-  <UserLayout> Content Data </UserLayout>
+  <UserLayout>
+    <div class="hero bg-base-200 h-96 mx-4">
+      <div class="hero-content text-center">
+        <div class="max-w-md">
+          <h1 class="text-5xl font-bold">Hello there</h1>
+          <p class="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button class="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+
+    <!-- product card -->
+    <div
+      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 my-4 mx-4"
+    >
+      <div
+        v-for="items in [1, 2, 3, 4, 5, 6, 7, 8]"
+        class="card bg-gray-100 w-full shadow-sm"
+      >
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes"
+          />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">Card Title</h2>
+          <p>
+            A card component has a figure, a body part, and inside body there
+            are title and actions parts
+          </p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </UserLayout>
 </template>
